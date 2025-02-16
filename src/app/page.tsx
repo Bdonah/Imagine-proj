9607914a452c9a5a1c329951b16daa112f609b58
Bdonah <bdonah1216@gmail.com>
2025-02-16 00:40:48 -0500

    fix: fix allignment of button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ function ClickCounter (){
   const [count, setCount] = useState(0);
 
   return (
-    <div className=" flex justify-center text-center mt-4">
+    <div className="flex flex-col items-center text-center mt-4">
       <button
         className="w-44 h-44  bg-[#D2B48C] text-black rounded-full flex items-center justify-center shadow-md hover:bg-[#C2A178]"
         onClick={() => setCount(count + 1)}
