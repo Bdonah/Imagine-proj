0246d0f3fe16d662dcb05dd29be46a8da610b760
Bdonah <bdonah1216@gmail.com>
2025-02-16 00:53:19 -0500

    fix: correctlt change font size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,10 +17,10 @@ function ClickCounter (){
   return (
     <div className="flex flex-col items-center text-center mt-4">
       <button
-        className="w-44 h-44  bg-[#D2B48C] text-black text 3xl font-sans rounded-full flex items-center justify-center shadow-md hover:bg-[#C2A178]"
+        className="w-44 h-44  bg-[#D2B48C] text-black text-3xl font-sans rounded-full flex items-center justify-center shadow-md hover:bg-[#C2A178]"
         onClick={() => setCount(count + 1)}
       >
-        Click for a cookie. 
+        🍪Click for a cookie🍪 
       </button>
       <h1 className="text-4xl mt-4">You have {count} cookies!</h1>
     </div>
