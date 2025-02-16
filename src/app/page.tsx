a473eee61fb76f3ac0c6a693d0f9f9a18084f0fe
Bdonah <bdonah1216@gmail.com>
2025-02-16 00:05:31 -0500

    feat: add clicker button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
-
+import { useState } from "react";
 
 export default function Home() {
-  return (<>
+  return (
+    <>
     <h1 className="text-2xl"> Emagine Educational Project</h1>
-    <h1> Welcome</h1>
+    <ClickCounter />
     </>
+  );
+}
+
+function ClickCounter (){
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="text-center mt-4">
+      <button
+        className="px-4 py-2 bg-blue-500 text-white rounded"
+        onClick={() => setCount(count + 1)}
+      >
+        Clicked {count} times
+      </button>
+    </div>
   );
 }
